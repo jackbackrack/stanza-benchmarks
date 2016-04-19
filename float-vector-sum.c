183d685f70ac7b399429f7aa9e6a71ef5c0f1644
jackbackrack <jrb@pobox.com>
2016-04-18 18:16:07 -0700

    add more general benchmarking facility + two more benchmarks

---
+++ float-vector-sum.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+float benchmark (int n, float* v, int len) {
+  float sum = 0.0;
+  for (int j = 0; j < n; j++) 
+    for (int i = 0; i < len; i++)
+      sum = sum + v[i];
+  return sum;
+}
+
+int main (int argc, char* argv[]) {
+  const int iters = atoi(argv[1]);
+  clock_t begin = clock();
+  const int n = 1000;
+  float* v  = (float*)malloc(n * sizeof(float));
+  for (int i = 0; i < n; i++) v[i] = 0.1;
+  float res = benchmark(iters, v, n);
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  if (res > 0.0)
+    printf("%f\n", time_spent);
+}
---
+++ point-sum.cpp
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+class vec_t {
+ public:
+  float x, y, z;
+  vec_t (const float x, const float y, const float z) : x(x), y(y), z(z) {}
+  
+  vec_t operator+(const vec_t& b) const {
+    return vec_t(x + b.x, y + b.y, z + b.z);
+  }
+};
+
+vec_t benchmark (int n, const vec_t& p) {
+  vec_t sum(0.0, 0.0, 0.0);
+  for (int i = 0; i < n; i++) 
+    sum = sum + p;
+  return sum;
+}
+
+int main (int argc, char* argv[]) {
+  const int iters = atoi(argv[1]);
+  clock_t begin = clock();
+  const vec_t p = vec_t(0.1, 0.2, 0.3);
+  vec_t res = benchmark(iters, p);
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  if (res.x > 0.0)
+    printf("%f\n", time_spent);
+}
